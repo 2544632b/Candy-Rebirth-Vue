c673aa7915f37c08aa08fd387d57b52f55791f98
little fairy <2578997513@qq.com>
2023-07-09 22:12:16 +0800

    Profile hidden fixed (not finish)

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,26 +1,45 @@
 <template>
     <div class="bg" style="background-image:url(https://i.loli.net/2018/04/15/5ad2ca39da9bb.jpg);"></div>
     <div class="notify-container"></div>
-    <div id="archive" class="screen archive move-show">
-        <div class="archive-container">
-            <div class="author-info right-in-animation" style="opacity: 1;">
+    <div class="menu">
+        <div class="group">
+            <a class="item mdi mdi-arrow-left hidden" id="back" @click=""></a>
+            <div class="drop-down">
+                <span class="item mdi mdi-format-annotation-plus" @click=""></span>
+                <span class="item mdi mdi-share-variant"></span>
+            </div>
+        </div>
+        <div class="group">
+            <div class="hidden">
+                <a class="item" @click="">关于</a>
+            </div>
+            <a @click="" class="item mdi mdi-menu"></a>
+            <a @click="activeSearch()" class="item mdi mdi-magnify"></a>
+            <div class="search">
+                <input class="search-box" name="search" id="search" ref="search" placeholder="回车Go！">
+            </div>
+        </div>
+    </div>
+    <div id="archive" class="screen archive move-show" v-if="profile">
+        <div class="archive-container" ref="container"  @scroll="handleScroll">
+            <div class="author-info right-in-animation" ref="x_profile">
                 <img class="avatar" src="../assets/223922884aeedb45bf77b249f183b5f3_8624852438576176182.jpg" alt="yanqing" width="100" height="100">
-                <div class="name">Hello World</div>
-                <div class="desc">自喻：仙人</div>
+                <div class="name">{{ profile.profile_name }}</div>
+                <div class="desc">{{ profile.blog_description }}</div>
                 <div class="stat">
-                    <div class="article-count">2</div>
-                    <div class="category-count">1</div>
-                    <div class="comment-count">1</div>
+                    <div class="article-count">{{ profile.article_counts }}</div>
+                    <div class="category-count">{{ profile.categories }}</div>
+                    <div class="comment-count">{{ profile.comments }}</div>
                 </div>
                 <div class="copyright">
-                    © Hello World 2021, 2022, 2023
+                    &copy; {{ profile.profile_name }} 2021, 2022, 2023
                     <div class="theme-info">
                         Theme <a href="https://github.com/Archeb/Candy-Rebirth">Candy-Rebirth</a> By <a href="https://mozz.ie/">Archeb</a>.
                     </div>
                 </div>
             </div>
             <div class="article-list">
-                <template v-for="t in templates">
+                <template v-for="t in articles">
                     <template v-if="t.photo">
                         <!--With photo-->
                         <div class="article-with-preview right-in-animation" :style="{'animation-duration':''+(default_num+=0.1)+'s'}">
@@ -51,7 +70,7 @@
                                 </div>
                             </div>
                             <div class="content" @click="">
-                                <p>{{ t.content }}</p>
+                                <p v-html="t.content"></p>
                             </div>
                         </div>
                     </template>
@@ -80,73 +99,74 @@
     }
 </style>
 <script lang="ts" name="XzjLvu87Fq">
-    import { defineComponent, reactive, ref } from 'vue';
+    import { defineComponent, reactive, ref, onMounted, onUnmounted, onBeforeUnmount } from 'vue';
     import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
     import VueCookies from 'vue-cookies';
 
     interface FormState {
-        password: string;
-        newPassword: string;
+        searchKey: string;
     }
     export default defineComponent({
         setup() {
             const formState = reactive<FormState>({
-                password: '',
-                newPassword: ''
+                searchKey: ''
             });
 
-            const title = ref('');
-            const another = ref('');
-            const category = ref('');
-            const date = ref('');
-            const content = ref('');
-            const photo = ref('');
+            const articles = ref('');
+            const profile = ref('');
 
             const default_num = 0.5;
 
-            const templates = [
-                {
-                    "title": "123",
-                    "photo": "https://i.loli.net/2018/04/15/5ad2ca39da9bb.jpg",
-                    "content": "测试内容",
-                    "category": "默认分类",
-                    "date": "2023/7/7 - 16:06",
-                    "another": "yanqing"
-                },
-                {
-                    "title": "123 - 1",
-                    "content": "欢迎使用Vue",
-                    "category": "默认分类",
-                    "date": "2023/7/7 - 16:07",
-                    "another": "yanqing"
-                },
-                {
-                    "title": "123 - 2",
-                    "photo": "https://i.loli.net/2018/04/15/5ad2ca39da9bb.jpg",
-                    "content": "欢迎使用Typecho, repeatcontent, repeatcontent, repeatcontent, repeatcontent, repeatcontent,",
-                    "category": "默认分类",
-                    "date": "2023/7/7 - 18:11",
-                    "another": "yanqing"
-                }
-            ]
+            const search = ref(null);
+            const activeSearch = () => {
+                search.value.focus();
+            }
 
-            axios.get("http://localhost:8871/arcives")
+            axios.get("http://localhost:8000/profile")
                 .then((response) => {
-                    title.value = response.data.title;
-                    another.value = response.data.another;
-                    category = response.data.description;
-                    date.value = response.data.date;
-                    content.value = response.data.content;
-                    photo.value = response.data.photo;
+                    profile.value = response.data;
                 })
                 .catch((error) => {
-                    console.log("Error pulling the archives!")
+                    console.log("拉取失败>_<!")
                 })
+
+            axios.get("http://localhost:8000/archives")
+                .then((response) => {
+                    articles.value = response.data;
+                })
+                .catch((error) => {
+                    console.log("拉取失败>_<!")
+                })
+
+            // From m.js:7
+            // 从0开始，到300px完全消失
+            const container = ref('');
+            const x_profile = ref('');
+            const isNonePointer = false;
+            const handleScroll = (event) => {
+                const scrollX = container.value.scrollLeft;
+                if (scrollX >= 300) {
+                    screenX = 300;
+                }
+                var hidden_value = 1 - scrollX / 300;
+                x_profile.value.style.opacity = hidden_value;
+            }
+
             return {
                 formState,
-                templates,
-                default_num
+                profile,
+                articles,
+
+                default_num,
+
+                search,
+                activeSearch,
+
+                container,
+                x_profile,
+                isNonePointer,
+                handleScroll
             };
-        },
+        }
     });
 </script>
